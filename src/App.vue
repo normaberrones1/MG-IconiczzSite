526ee503a0ea4dbe2c95edaffd3eab512394363f
normaberrones1 <normaberrones.tx@gmail.com>
2024-10-18 17:55:25 -0400

    Router

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,13 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Mike Gonzalez & The Iconiczz" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/shows">Shows</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+        <RouterLink to="/merch">Merch</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/MerchView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="merch">
+      <h1>This is a merch page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .merch {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
